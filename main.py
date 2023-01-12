045fa0eb5988873cc9a67a800f44855a84f63910
Elliott Smith <117014763+ElliottSmith610@users.noreply.github.com>
2023-01-12 15:03:13 +0000

    Sort all extensions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Think of something you melt
 import os
 import shutil
 
@@ -8,24 +7,67 @@ load_dotenv()
 
 source_folder = os.environ.get('SOURCE_FOLDER')
 
+pictures = ["jpg", "jpeg", "png", "bmp", "gif", "tiff", "psd"]
+audio = ["pcm", "wav", "aiff", "mp3", "aac", "ogg", "wma", "flac", "alac"]
+video = ["mp4", "mov", "wmv", "webm", "avi", "flv", "mkv", "mts", "mpeg4"]
+docs = ["txt", "doc", "docx", "rtf", "pdf"]
+apps = ["exe", "zip", "rar", "jar"]
+
+directories = {
+    "pictures": pictures,
+    "audio": audio,
+    "video": video,
+    "docs": docs,
+    "apps-zips": apps,
+}
+
 for file_name in os.listdir(source_folder):
 
     source = f"{source_folder}/{file_name}"
     if os.path.isfile(source):
+
+        folder = None
+        extension = None
+
         try:
             file_name.split(".")[1]
         except IndexError:
-            extension = "other"
+            folder = "other"
         else:
             extension = file_name.split(".")[-1]
 
-        destination = f"{source_folder}/{extension}/{file_name}"
+            folder = "other"
+            for key, value in directories.items():
+                if extension.lower() in directories[key]:
+                    folder = key
+
+            if folder == "other":
+                extension = None
+
+        if folder is None:
+            destination = f"{source_folder}/{file_name}"
+        elif extension is None:
+            destination = f"{source_folder}/{folder}/{file_name}"
+        else:
+            destination = f"{source_folder}/{folder}/{extension}/{file_name}"
 
         try:
             shutil.move(source, destination)
         except FileNotFoundError:
-            os.mkdir(f"{source_folder}/{extension}")
-            shutil.move(source, destination)
+
+            if extension is None:
+                os.mkdir(f"{source_folder}/{folder}")
+                shutil.move(source, destination)
+            else:
+
+                try:
+                    os.mkdir(f"{source_folder}/{folder}/{extension}")
+                except FileNotFoundError:
+                    os.mkdir(f"{source_folder}/{folder}")
+                    os.mkdir(f"{source_folder}/{folder}/{extension}")
+                finally:
+                    shutil.move(source, destination)
+
         finally:
             print(f"Moved: {file_name}")
 
